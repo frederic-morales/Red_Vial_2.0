34d4ea72439680ca3f6b5ee4ff298739ab8cc3bf
Frederic Morales <tocomorales30@gmail.com>
2025-05-07 21:17:36 -0600

    Creando el proyecto

---
+++ Red Vial 2.0/NodoInterseccion.cs
@@ -0,0 +1,16 @@
+﻿namespace Red_Vial_2._0
+{
+    public class NodoInterseccion
+    {
+        public InformacionNodo Info { get; set; }
+        public NodoInterseccion Norte { get; set; }
+        public NodoInterseccion Sur { get; set; }
+        public NodoInterseccion Este { get; set; }
+        public NodoInterseccion Oeste { get; set; }
+
+        public NodoInterseccion(InformacionNodo info)
+        {
+            Info = info;
+        }
+    }
+}
