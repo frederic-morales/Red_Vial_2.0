c05cf698d41f03551b6d9de353ed7b2b3d71c275
Frederic Morales <tocomorales30@gmail.com>
2025-05-09 22:55:03 -0600

    Agregando carros

---
+++ Red Vial 2.0/CarroInfo.cs
@@ -0,0 +1,19 @@
+﻿namespace Red_Vial_2._0
+{
+    public class CarroInfo
+    {
+        private static int _ultimoId = 0;  
+        public int idCarro { get; }  
+
+        public int width { get; set; }
+
+        public int height { get; set; }
+
+        public CarroInfo(int width, int height)
+        {
+            this.idCarro = ++_ultimoId;  
+            this.width = width;
+            this.height = height;
+        }
+    }
+}
--- Red Vial 2.0/RedVial.cs
+++ Red Vial 2.0/RedVial.cs
@@ -4,9 +4,12 @@
     {
         public NodoInterseccion NodoCentral { get; set; }
 
+        public List<CarroInfo> carrosTotales { get; set; }  // Cambia a List<>
+
         public RedVial()
         {
             NodoCentral = null;
+            carrosTotales = new List<CarroInfo>();  // Inicializa como lista
         }
 
         public void CrearNodoCentral(InformacionNodo info)
@@ -40,5 +43,56 @@
 
             return nuevo;
         }
+
+        public void AgregarCarro(int width, int height)
+        {
+            CarroInfo nuevoCarro = new CarroInfo(width, height);
+            carrosTotales.Add(nuevoCarro);  // Método más simple
+        }
+
+        public bool MoverCarroAlNorte(int idCarro, int cantidad)
+        {
+            var carro = carrosTotales.FirstOrDefault(c => c.idCarro == idCarro);
+            if (carro != null)
+            {
+                carro.height -= cantidad; // Restar para mover hacia arriba (norte)
+                return true;
+            }
+            return false;
+        }
+
+        public bool MoverCarroAlSur(int idCarro, int cantidad)
+        {
+            var carro = carrosTotales.FirstOrDefault(c => c.idCarro == idCarro);
+            if (carro != null)
+            {
+                carro.height += cantidad; // Sumar para mover hacia abajo (sur)
+                return true;
+            }
+            return false;
+        }
+
+        public bool MoverCarroAlOeste(int idCarro, int cantidad)
+        {
+            var carro = carrosTotales.FirstOrDefault(c => c.idCarro == idCarro);
+            if (carro != null)
+            {
+                carro.width -= cantidad; // Restar para mover hacia la izquierda (oeste)
+                return true;
+            }
+            return false;
+        }
+
+        public bool MoverCarroAlEste(int idCarro, int cantidad)
+        {
+            var carro = carrosTotales.FirstOrDefault(c => c.idCarro == idCarro);
+            if (carro != null)
+            {
+                carro.width += cantidad; // Sumar para mover hacia la derecha (este)
+                return true;
+            }
+            return false;
+        }
+
     }
 }
